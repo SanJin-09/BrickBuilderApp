eac5ff078768ebdf1affdf93148b018d2596d065
SanJin_Mac <p3032237762@outlook.com>
2025-06-25 01:44:46 +0800

    Initial Commit

---
+++ BrickBuilderApp/BrickBuilderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrickBuilderAppApp.swift
+//  BrickBuilderApp
+//
+//  Created by San 金 on 2025/6/25.
+//
+
+import SwiftUI
+
+@main
+struct BrickBuilderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrickBuilderAppTests/BrickBuilderAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BrickBuilderAppTests.swift
+//  BrickBuilderAppTests
+//
+//  Created by San 金 on 2025/6/25.
+//
+
+import Testing
+@testable import BrickBuilderApp
+
+struct BrickBuilderAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
